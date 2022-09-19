54b0c27a725875cdf3974b61eba209d0c7dfaef2
Esteban Macanek <estebanmacanek@hotmail.com>
2022-09-19 17:06:02 -0300

    Add playRound()

--- javascript.js
+++ javascript.js
@@ -20,3 +20,32 @@ function getComputerChoice () {
         return 'Scissors'
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors"
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors"
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper"
+    }
+    else if (playerSelection === computerSelection) {
+        return "Tie!"
+    }
+    else {
+        return "Error!"
+    }
+}
