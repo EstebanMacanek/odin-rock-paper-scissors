df50dc54860baa68b9d8d97999fa16ebcdfdf3ce
Esteban Macanek <estebanmacanek@hotmail.com>
2022-09-19 18:21:12 -0300

    Add fromIntToSelection()

--- javascript.js
+++ javascript.js
@@ -8,3 +8,15 @@ function getRandomLessThan3 () {
         return getRandomLessThan3()
     }
 }
+
+function fromIntToSelection (int) {
+    if (int === 0) {
+        return 'Rock'
+    }
+    else if (int === 1) {
+        return 'Paper'
+    }
+    else if (int === 2) {
+        return 'Scissors'
+    }
+}
